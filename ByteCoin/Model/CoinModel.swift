b88ef03a628177de38c2276aea32d08f847f9485
alexliou <alexliou0@gmail.com>
2022-08-20 18:40:38 -0500

    Finished app.

---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Alex Liou on 8/20/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinModel {
+    let price: Double
+}
